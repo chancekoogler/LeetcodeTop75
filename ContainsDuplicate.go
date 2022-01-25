a543276d1031119742d4a4c1e736a075e2dcb74c
Chance Koogler <chance.koogler@neudesic.com>
2022-01-25 16:56:40 -0500

    initial commit

---
+++ ContainsDuplicate.go
@@ -0,0 +1,12 @@
+package containsDuplicate
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]int)
+	for i := range nums {
+		if _, ok := m[nums[i]]; ok {
+			return true
+		}
+		m[nums[i]] = i
+	}
+	return false
+}
