b242c8c63aae0650e3e94f5cbafe7b72a98483a2
Chance Koogler <chance.koogler@neudesic.com>
2022-01-26 14:39:42 -0500

    Added further table driven testing

--- contains_duplicate/contains_duplicate_test.go
+++ contains_duplicate/contains_duplicate_test.go
@@ -1,11 +1,33 @@
 package leetcode
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestContainsDuplicate(t *testing.T) {
 	nums := []int{8, 2, 3, 2, 1, 4}
 	ans := ContainsDuplicate(nums)
-	if ans != true {
+	if !ans {
 		t.Errorf("Got %t. Expected: true", ans)
+	}
+}
+
+func TestContainsDuplication(t *testing.T) {
+	testNums := [][]int{
+		{1, 2, 3, 4, 1},
+		{3, 4, 5, 6, 7, 3},
+		{2, 3, 5, 7, 8, 8},
+		{600, 601, 600},
+	}
+	// fmt.Println(testNums)
+	for i := range testNums {
+		testName := fmt.Sprintf("Test %d", i)
+		t.Run(testName, func(t *testing.T) {
+			ans := ContainsDuplicate(testNums[i])
+			if !ans {
+				t.Errorf("Got %t. Expected: true", ans)
+			}
+		})
 	}
 }
--- two_sum/two_sum_test.go
+++ two_sum/two_sum_test.go
@@ -1,12 +1,29 @@
 package leetcode
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestTwoSum(t *testing.T) {
-	nums := []int{2, 11, 7, 15}
-	target := 9
-	ans := TwoSum(nums, target)
-	if ans[0] != 2 && ans[1] != 0 {
-		t.Errorf("TwoSum([2,11,7,15], 9): got: %d; want {0,2}", ans)
+	var testNums = []struct {
+		a, b, c, d int
+		want       int
+	}{
+		{2, 11, 7, 15, 22},
+		{3, 6, 12, 11, 18},
+		{1, 3, 2, 5, 5},
+		{1, 10, 25, 50000, 35},
+	}
+	for i, val := range testNums {
+		testName := fmt.Sprintf("Test %d. Using %d, %d, %d, %d", i, val.a, val.b, val.c, val.d)
+		t.Run(testName, func(t *testing.T) {
+			nums := []int{val.a, val.b, val.c, val.d}
+			ans := TwoSum(nums, val.want)
+			if nums[ans[0]]+nums[ans[1]] != val.want {
+				t.Errorf("got %d, want %d", ans, val.want)
+			}
+
+		})
 	}
 }
