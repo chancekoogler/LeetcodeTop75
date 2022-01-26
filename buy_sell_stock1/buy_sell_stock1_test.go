0066068ecae1098075d2379ef8791fcc3757dc3d
Chance Koogler <chance.koogler@neudesic.com>
2022-01-26 16:44:06 -0500

    Add partial subtests for max profit function

--- buy_sell_stock1/buy_sell_stock1_test.go
+++ buy_sell_stock1/buy_sell_stock1_test.go
@@ -2,10 +2,39 @@ package leetcode
 
 import "testing"
 
+type testNums []struct {
+	nums []int
+	want int
+}
+
 func TestMaxProfit(t *testing.T) {
 	nums := []int{7, 1, 5, 3, 6, 4}
 	ans := MaxProfit(nums)
 	if ans != 5 {
 		t.Errorf("Expected 5, got %v", ans)
 	}
+}
+
+func TestMaxProfits(t *testing.T) {
+	var testNums = []struct {
+		nums []int
+		want int
+	}{
+		{
+			nums: []int{7, 1, 5, 3, 6, 4},
+			want: 5,
+		},
+		{
+			nums: []int{7, 2, 5, 3, 6, 4},
+			want: 4,
+		},
+		{
+			nums: []int{7, 3, 5, 3, 6, 4},
+			want: 3,
+		},
+	}
+
+	// for i := range testNums {
+
+	// }
 }
