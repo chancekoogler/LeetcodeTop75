93ebe9758ac17e9aca0ea507fc576ae58d37cc10
Chance Koogler <chance.koogler@neudesic.com>
2022-01-26 11:45:07 -0500

    Third push test

---
+++ buy_sell_stock1.go
@@ -0,0 +1,17 @@
+package stock1
+
+func maxProfit(prices []int) int {
+	var profit int
+	min := prices[0]
+	for i := range prices {
+		if prices[i] < min {
+			min = prices[i]
+		} else {
+			tmp := prices[i] - min
+			if tmp > profit {
+				profit = tmp
+			}
+		}
+	}
+	return profit
+}
---
+++ contains_duplicate.go
@@ -0,0 +1,12 @@
+package ContainsDuplicate
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]int)
+	for i := range nums {
+		if _, ok := m[nums[i]]; ok {
+			return true
+		}
+		m[nums[i]] = i
+	}
+	return false
+}
